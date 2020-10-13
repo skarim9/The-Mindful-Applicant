56b64fc92d30990975a46b49180a5514c7f860f9
Sandra Zhen <szhen8930@gmail.com>
2020-10-13 01:06:32 -0400

    sliding scales modified

--- src/quiz/QuizQuestion.tsx
+++ src/quiz/QuizQuestion.tsx
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import './quiz.scss'
 import Slider from '@material-ui/core/Slider';
 
+const PrettoSlider = withStyles({
+  root: {
+    color: "#595959ff",
+    height: 8
+  },
+  thumb: {
+    height: 25,
+    width: 25,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit"
+    }
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)"
+  },
+  mark: {
+    backgroundColor: '#4593C4',
+    height: 35,
+    width: 35,
+    borderRadius: 30,
+    marginTop: -13,
+    marginLeft: -17,
+  },
+  track: {
+    height: 0,
+    borderRadius: 4
+  },
+  rail: {
+    marginTop:-4,
+    height: 16,
+    borderRadius: 4
+  }
+})(Slider);
 export default class QuizQuestion extends Component <IAppProps,IAppState>{
   
 constructor(props: IAppProps){
@@ -14,15 +53,17 @@ constructor(props: IAppProps){
     return (
       <div className = "quiz-question-container">
           <div>
-    <p>{this.props.question}</p>
-          <Slider
-            defaultValue={3}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={1}
-            marks
-            min={1}
-            max={5}
+          <p>{this.props.question}</p>
+          
+            <PrettoSlider
+              valueLabelDisplay="auto"
+              
+              aria-labelledby="discrete-slider"
+              defaultValue={20}
+              step={1}
+              marks
+              min={1}
+              max={5}
             />
         
         </div>
@@ -36,3 +77,4 @@ interface IAppProps{
 interface IAppState{
     name:string;
 }
+
