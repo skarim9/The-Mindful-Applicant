c40d59173c875473583e9de7cb50ca0cf75d6340
Sandra Zhen <szhen8930@gmail.com>
2020-10-16 21:28:45 -0400

    added links for temporary presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Quiz from './quiz/Quiz';
@@ -10,6 +10,12 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <div>
+        <nav>
+             <Link  style = {{color:"white"}} to="/quiz">Quiz</Link>
+             <Link  style = {{color:"white"}} to="/points-allocation">Points Allocation</Link>
+           
+             
+         </nav>
             <Switch>
              <Route path="/points-reallocation" component={PointsAllocation} />
              <Route path="/quiz" component={Quiz}/>
