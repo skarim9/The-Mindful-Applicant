8350cf87b45ae7103fdb28185bfa8365dfc4849c
Shihab Karim <shihabkarim6@gmail.com>
2020-11-16 14:54:17 -0500

    Finished frontend of Signup and Login Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
 // database imports
-import Firebase from 'firebase';
 
 import './App.css';
 import Quiz from './quiz/Quiz';
 
 import Dashboard from './profile/index';
 import PointsAllocation from './points-allocation/PointsAllocation';
-import SignIn from './login/login'
+import SignIn from './login/login';
+import SignUp from './signup/Signup';
 
-import {addNewUser,addOriginalQuizResult,addReallocatedQuizResult} from './firebase-db/firestore/db-functions'
-
-class App extends React.Component {
-
-  constructor(props:any){
-    super(props);
-    this.state = {
-      users: []
-    }
-  }
-
-  componentDidMount() {
-  }
-  componentDidUpdate(prevProps:any, prevState:any) {
-    // check on previous state
-    // only write when it's different with the new state
-    if (prevState !== this.state) {
-      
-    }
-  }
-  addUser(){
-    addNewUser('szhen@gmail.com');
-  }
-  addToDB(){
-    addOriginalQuizResult(
-      {date: new Date(),
-      quiz:{
-        decision_making:389472
-      }},
-      "SANDRA"
-    )
-    addReallocatedQuizResult(
-      {date: new Date(),
-      quiz:{
-        decision_making:389472
-      }},
-      "SANDRA"
-    )
-  }
-  render(){
-    return (
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
      
         <BrowserRouter>
           <div>
@@ -61,11 +22,9 @@ class App extends React.Component {
                <Route path="/quiz" component={Quiz}/>
                <Route path ="/profile" component={Dashboard}/>
                <Route path="/signin" component={SignIn}/>
+               <Route path="/signup" component={SignUp}/>
                
              </Switch>
-             <button onClick = {this.addUser}>Click to add user</button>
-             
-             <button onClick = {this.addToDB}>Click to add quiz result</button>
              
           </div> 
         </BrowserRouter>
@@ -76,6 +35,5 @@ class App extends React.Component {
   
     );
   }
-}
 
 export default App;
