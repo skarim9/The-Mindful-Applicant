6f5c23960e0945c96ad2155f10c862b8fb34de5f
Shihab Karim <shihabkarim6@gmail.com>
2020-11-30 21:44:22 -0500

    Added travis script

---
+++ travis_merge_script.sh
@@ -0,0 +1,10 @@
+if [ "$TRAVIS_BRANCH" != "feature_" ]; then 
+    exit 0;
+fi
+export GIT_COMMITTER_EMAIL="<your-email>"
+export GIT_COMMITTER_NAME="<your-name>"
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+git checkout master || exit
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+git push @github.com/">https://${GITHUB_TOKEN}@github.com/<your-github-user>/<your-repository-name>.git
