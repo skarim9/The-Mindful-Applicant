cd9a10def9ed0d6729eb7fbe03d76afc64f170b3
Shihab Karim <shihabkarim6@gmail.com>
2020-11-30 22:35:11 -0500

    Attempt 4

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -5,6 +5,6 @@ export GIT_COMMITTER_EMAIL="<your-email>"
 export GIT_COMMITTER_NAME="<your-name>"
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
-git checkout master || exit
+git checkout main || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
 git push @github.com/">https://${GITHUB_TOKEN}@github.com/skarim9/The-Mindful-Applicant.git
