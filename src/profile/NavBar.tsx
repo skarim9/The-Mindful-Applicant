81ff2b103394cc1525d643f4470900e9a115f0cd
yoontsy2000 <yoontsy2000@gmail.com>
2020-12-07 11:19:48 -0500

    Fixed navbar font and hovering bug, still need to implement drawer

--- src/profile/NavBar.tsx
+++ src/profile/NavBar.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+
 import MenuIcon from '@material-ui/icons/Menu';
-import {auth} from '../firebase-db/config'
 
 import Grid from '@material-ui/core/Grid'
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { Title } from '@material-ui/icons';
+import { CssBaseline } from '@material-ui/core';
+
+import {auth} from '../firebase-db/config'
 
 const theme = createMuiTheme({
   palette: {
@@ -42,6 +51,9 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
       marginLeft: 'auto'
     },
+    title: {
+      flexGrow: 1,
+    },
     menuButton: {
       marginRight: theme.spacing(2),
     },
@@ -59,13 +71,11 @@ export default function Navbar() {
             <IconButton>
               <MenuIcon />
             </IconButton>
-
-            <Button onClick = {() => { auth.signOut()}}>Sign Out</Button>
-
+            <Box className={classes.title}/>
+            <Button  onClick = {() => {auth.signOut()}}><Typography>Sign Out</Typography></Button>
           </Toolbar>
         </AppBar>
       </MuiThemeProvider>
     </div>
-
   );
 }
